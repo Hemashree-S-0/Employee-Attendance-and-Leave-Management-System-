0f56beb01bfdb9dd254c931b337c1a4e2f3733c4
Hemashree-S-0 <168103527+Hemashree-S-0@users.noreply.github.com>
2026-01-11 22:56:08 +0530

    Create EmployeeDashboard.tsx

---
+++ Frontend/src/Pages/EmployeeDashboard.tsx
@@ -0,0 +1,9 @@
+export default function EmployeeDashboard() {
+  return (
+    <div>
+      <h1>Employee Dashboard</h1>
+      <button>Punch In</button>
+      <button>Punch Out</button>
+    </div>
+  );
+}
